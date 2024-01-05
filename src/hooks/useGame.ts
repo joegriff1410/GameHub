54d1688ecb6298238a6e0f237e9f0b7390447b87
Joe Griffin <jgriffin1410@gmail.com>
2024-01-05 05:31:10 +0000

    Refactor - move entity interfaces

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,13 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-client";
-import { Game } from "./useGames";
+import { useQuery } from '@tanstack/react-query';
+import APIClient from '../services/api-client';
+import { Game } from '../entities/Game';
 
 // TODO: fix dependency of game interface
 const apiClient = new APIClient<Game>('/games');
 
-const useGame = (slug: string) => useQuery({
-    queryKey: ['games', slug],
-    queryFn: () => apiClient.get(slug)
-});
+const useGame = (slug: string) =>
+    useQuery({
+        queryKey: ['games', slug],
+        queryFn: () => apiClient.get(slug),
+    });
 
 export default useGame;
